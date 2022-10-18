2f6d85be70babe4b7b3dd2203b7f8b3d48f9a6f6
Vanodium <vanodium@ya.ru>
2022-10-18 20:05:45 +0400

    Added .env

--- DjangoTask1/DjangoTask1/settings.py
+++ DjangoTask1/DjangoTask1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-     'django-insecure-tp(4!77fzn9#z6pao0*i+mm9rk3^w@k*#o9pw%+fagurh2al73'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
