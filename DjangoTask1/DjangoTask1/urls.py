6148135916ab9396c742fa67ba83586f94bd28f6
Vanodium <vanodium@ya.ru>
2022-10-19 16:43:50 +0400

    Fixed filenames cases

--- DjangoTask1/DjangoTask1/urls.py
+++ DjangoTask1/DjangoTask1/urls.py
@@ -1,4 +1,4 @@
-"""DjangoTask1 URL Configuration
+"""djangotask1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
