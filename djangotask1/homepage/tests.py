13d892a40fcc7544be00ea6042625048c0810b7e
Vanodium <94909357+Vanodium@users.noreply.github.com>
2022-10-25 19:50:02 +0400

    Update tests.py

--- djangotask1/homepage/tests.py
+++ djangotask1/homepage/tests.py
@@ -1,3 +1,25 @@
-# from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class StaticURLTests(TestCase):
+    def test_homepage(self):
+        response = Client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        response = Client().get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_catalog(self):
+        response = Client().get('/catalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_catalog_item(self):
+        response = Client().get('/catalog/123')
+        self.assertEqual(response.status_code, 200)
+
+        response = Client().get('/catalog/-123')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/string')
+        self.assertEqual(response.status_code, 404)
