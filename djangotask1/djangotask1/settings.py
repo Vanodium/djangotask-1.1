d0580a1a2478378457a48f655f23d4094782ae1d
Vanodium <94909357+Vanodium@users.noreply.github.com>
2022-10-25 19:47:56 +0400

    Update settings.py

--- djangotask1/djangotask1/settings.py
+++ djangotask1/djangotask1/settings.py
@@ -15,10 +15,8 @@ from pathlib import Path
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
