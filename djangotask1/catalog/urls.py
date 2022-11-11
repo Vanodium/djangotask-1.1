9651edf3af87fb9c774def74a416946da7748294
Vanodium <vanodium@ya.ru>
2022-11-11 18:13:53 +0400

    1234567890

--- djangotask1/catalog/urls.py
+++ djangotask1/catalog/urls.py
@@ -4,6 +4,6 @@ from catalog import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.item_list, name='item_detail'),
+    path('', views.item_list, name='item_list'),
     re_path(r'^(?P<pk>[1-9][0-9]*?)/$', views.item_detail)
 ]
